857ac53cf92d421f61ca391e402d0927832202cf
Ayrton Sepúlveda <ayrtonsepch@gmail.com>
2024-11-20 19:07:14 -0600

    Versión 13. Redirección desde el panel de admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App\Http\Responses\LogoutResponse;
 use Filament\Support\Colors\Color;
 use Filament\Support\Facades\FilamentColor;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Filament\Http\Responses\Auth\Contracts\LogoutResponse as LogoutResponseContract;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
-
+        $this->app->bind(LogoutResponseContract::class, LogoutResponse::class);
     }
 }
